551225dd7d645585020f8e3af6d52f0b43a8376b
Lucy Chen <lucyc1225xx@gmail.com>
2021-09-10 23:43:56 -0400

    Update myFrame.java
    - Added ln, log, and sqrt buttons

--- GUI Calculator - Scientific/myFrame.java
+++ GUI Calculator - Scientific/myFrame.java
@@ -39,87 +39,112 @@ class myFrame implements ActionListener{
   JButton buttonDot;
   JButton buttonAC;
   JButton buttonSin;
+  JButton buttonCos;
+  JButton buttonTan;
+  JButton buttonLog;
+  JButton buttonLn;
+  JButton buttonSqrt;
 
   myFrame(){
     JFrame frame = new JFrame();
-    label.setBounds(5, 20, 388, 50);
+    label.setBounds(5, 20, 498, 50);
     label.setOpaque(true);
     label.setBackground(new Color(115, 147, 179));
 
     button0 = new JButton("0");
-    button0.setBounds(5, 313, 100, 50);//around 318 seems to the the max x and y
+    button0.setBounds(115, 313, 100, 50);
     button0.addActionListener(this);
 
     button1 = new JButton("1");
-    button1.setBounds(5, 258, 100, 50);
+    button1.setBounds(115, 258, 100, 50);
     button1.addActionListener(this);
 
     button2 = new JButton("2");
-    button2.setBounds(110, 258, 100, 50);
+    button2.setBounds(220, 258, 100, 50);
     button2.addActionListener(this);
 
     button3 = new JButton("3");
-    button3.setBounds(215, 258, 100, 50);
+    button3.setBounds(325, 258, 100, 50);
     button3.addActionListener(this);
 
     button4 = new JButton("4");
-    button4.setBounds(5, 203, 100, 50);
+    button4.setBounds(115, 203, 100, 50);
     button4.addActionListener(this);
 
     button5 = new JButton("5");
-    button5.setBounds(110, 203, 100, 50);
+    button5.setBounds(220, 203, 100, 50);
     button5.addActionListener(this);
 
     button6 = new JButton("6");
-    button6.setBounds(215, 203, 100, 50);
+    button6.setBounds(325, 203, 100, 50);
     button6.addActionListener(this);
 
     button7 = new JButton("7");
-    button7.setBounds(5, 148, 100, 50);
+    button7.setBounds(115, 148, 100, 50);
     button7.addActionListener(this);
 
     button8 = new JButton("8");
-    button8.setBounds(110, 148, 100, 50);
+    button8.setBounds(220, 148, 100, 50);
     button8.addActionListener(this);
 
     button9 = new JButton("9");
-    button9.setBounds(215, 148, 100, 50);
+    button9.setBounds(325, 148, 100, 50);
     button9.addActionListener(this);
 
     buttonAdd = new JButton("+");
-    buttonAdd.setBounds(320, 313, 73, 50);
+    buttonAdd.setBounds(430, 313, 73, 50);
     buttonAdd.addActionListener(this);
 
     buttonSubtract = new JButton("-");
-    buttonSubtract.setBounds(320, 258, 73, 50);
+    buttonSubtract.setBounds(430, 258, 73, 50);
     buttonSubtract.addActionListener(this);
 
     buttonMultiply = new JButton("x");
-    buttonMultiply.setBounds(320, 203, 73, 50);
+    buttonMultiply.setBounds(430, 203, 73, 50);
     buttonMultiply.addActionListener(this);
 
     buttonDivide = new JButton("/");
-    buttonDivide.setBounds(320, 148, 73, 50);
+    buttonDivide.setBounds(430, 148, 73, 50);
     buttonDivide.addActionListener(this);
 
     buttonEqual = new JButton("=");
-    buttonEqual.setBounds(215, 313, 100, 50);
+    buttonEqual.setBounds(325, 313, 100, 50);
     buttonEqual.addActionListener(this);
 
     buttonDot = new JButton(".");
-    buttonDot.setBounds(110, 313, 100, 50);
+    buttonDot.setBounds(220, 313, 100, 50);
     buttonDot.addActionListener(this);
 
     buttonAC = new JButton("AC");
-    buttonAC.setBounds(320, 93, 73, 50);
+    buttonAC.setBounds(430, 93, 73, 50);
     buttonAC.addActionListener(this);
 
-    buttonSin = new JButton("Sin");
-    buttonSin.setBounds(215, 93, 100, 50);
+    buttonSin = new JButton("sin");
+    buttonSin.setBounds(115, 93, 100, 50);
     buttonSin.addActionListener(this);
 
+    buttonCos = new JButton("cos");
+    buttonCos.setBounds(220, 93, 100, 50);
+    buttonCos.addActionListener(this);
+
+    buttonTan = new JButton("tan");
+    buttonTan.setBounds(325, 93, 100, 50);
+    buttonTan.addActionListener(this);
+
+    buttonLog = new JButton("log");
+    buttonLog.setBounds(10, 148, 100, 50);
+    buttonLog.addActionListener(this);
+
+    buttonLn = new JButton("ln");
+    buttonLn.setBounds(10, 203, 100, 50);
+    buttonLn.addActionListener(this);
+
+    buttonSqrt = new JButton("\u221A");
+    buttonSqrt.setBounds(10, 258, 100, 50);
+    buttonSqrt.addActionListener(this);
+
     frame.setTitle("GUI Calculator");
-    frame.setSize(400,400);
+    frame.setSize(540,400);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setResizable(false);
     frame.setVisible(true);
@@ -143,6 +168,11 @@ class myFrame implements ActionListener{
     frame.add(buttonAC);
     frame.add(label);
     frame.add(buttonSin);
+    frame.add(buttonCos);
+    frame.add(buttonTan);
+    frame.add(buttonLog);
+    frame.add(buttonLn);
+    frame.add(buttonSqrt);
   }
   public void actionPerformed(ActionEvent e){
     if (e.getSource() == button0){
@@ -150,7 +180,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "0";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "0";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -166,7 +201,7 @@ class myFrame implements ActionListener{
       if (continuescientificnum){
         scientificnum += "1";
         label.setText(" " + operator + " " + scientificnum);
-        System.out.println(scientificnum);
+        
       }
       else if(continuenum1){
         num1 += "1";
@@ -181,7 +216,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "2";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "2";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -194,7 +234,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "3";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "3";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -207,7 +252,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "4";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "4";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -220,7 +270,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "5";
+        label.setText(" " + operator + " " + scientificnum);
+       
+      }
+      else if(continuenum1){
         num1 += "5";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -233,7 +288,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "6";
+        label.setText(" " + operator + " " + scientificnum);
+       
+      }
+      else if(continuenum1){
         num1 += "6";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -246,7 +306,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "7";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "7";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -259,7 +324,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "8";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "8";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -272,7 +342,12 @@ class myFrame implements ActionListener{
         continuenum1 = true;
         continuenum2 = false;
       }
-      if(continuenum1){
+      if (continuescientificnum){
+        scientificnum += "9";
+        label.setText(" " + operator + " " + scientificnum);
+        
+      }
+      else if(continuenum1){
         num1 += "9";
         label.setText(" " + num1 + " " + operator + " " + num2);
       }else if (continuenum2){
@@ -368,6 +443,26 @@ class myFrame implements ActionListener{
         result = Math.sin(Math.toRadians(convertedScientificNum));
         label.setText(" " + operator + " " + scientificnum + " = " + result);
       }
+      else if (operator.equals("cos")){
+        result = Math.cos(Math.toRadians(convertedScientificNum));
+        label.setText(" " + operator + " " + scientificnum + " = " + result);
+      }
+      else if (operator.equals("tan")){
+        result = Math.tan(Math.toRadians(convertedScientificNum));
+        label.setText(" " + operator + " " + scientificnum + " = " + result);
+      }
+      else if (operator.equals("log")){
+        result = Math.log10(convertedScientificNum);
+        label.setText(" " + operator + " " + scientificnum + " = " + result);
+      }
+      else if (operator.equals("ln")){
+        result = Math.log(convertedScientificNum);
+        label.setText(" " + operator + " " + scientificnum + " = " + result);
+      }
+      else if (operator.equals("\u221A")){
+        result = Math.sqrt(convertedScientificNum);
+        label.setText(" " + operator + " " + scientificnum + " = " + result);
+      }
       else if (operator.equals("+")){
         result = convertedNum1 + convertedNum2;
         label.setText(" " + num1 + " " + operator + " " + num2 + " = " + result);
@@ -409,6 +504,31 @@ class myFrame implements ActionListener{
     }
     if (e.getSource() == buttonSin){
       operator = "sin";
+      continuescientificnum = true;
+      label.setText(operator);
+    }
+    if (e.getSource() == buttonCos){
+      operator = "cos";
+      continuescientificnum = true;
+      label.setText(operator);
+    }
+    if (e.getSource() == buttonTan){
+      operator = "tan";
+      continuescientificnum = true;
+      label.setText(operator);
+    }
+    if (e.getSource() == buttonLog){
+      operator = "log";
+      continuescientificnum = true;
+      label.setText(operator);
+    }
+    if (e.getSource() == buttonLn){
+      operator = "ln";
+      continuescientificnum = true;
+      label.setText(operator);
+    }
+    if (e.getSource() == buttonSqrt){
+      operator = "\u221A";
       continuescientificnum = true;
       label.setText(operator);
     }
